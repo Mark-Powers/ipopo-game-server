24bf8174ed1090421810a65c6edec64291e41d10
Mark Powers <markppowers0@gmail.com>
2018-06-28 10:05:53 -0400

    Fix tictactoe

--- games/game_tictactoe.py
+++ games/game_tictactoe.py
@@ -13,19 +13,28 @@ class Game(object):
     def __init__(self):
         self.board = []
         self.players = ['X', 'O']
-        self.turn = 0;
+        self.turn = 0
+        self.winner = ''
         
     @Validate
     def validate(self, context):
         for x in range(3):
             self.board.append(['_']*3)
+        print('TicTacToe Game has been loaded')
 
     @Invalidate
     def invalidate(self,context):
         self.board = None
 
+    def resetGame(self):
+        self.turn = 0
+        self.winner = ''
+        self.board = []
+        for x in range(3):
+            self.board.append(['_']*3)
+
     def setPos(self, r, c):
-        if(self.board[r][c] == '_'):
+        if(self.board[r][c] == '_' and self.winner == ''):
             self.board[r][c] = self.players[self.turn]
             self.turn += 1;
             self.turn %= 2;
@@ -35,22 +44,30 @@ class Game(object):
             if self.board[r][0] == self.board[r][1] \
                 and self.board[r][0] == self.board[r][2] \
                 and self.board[r][0] != '_':
+                self.winner = self.players[(self.turn + 1)%2]
                 return True
         for c in range(len(self.board[0])):
             if self.board[0][c] == self.board[1][c] \
                 and self.board[0][c] == self.board[2][c] \
                 and self.board[0][c] != '_':
+                self.winner = self.players[(self.turn + 1)%2]
                 return True
         if self.board[0][0] == self.board[1][1] \
            and self.board[0][0] == self.board[2][2] \
            and self.board[0][0] != '_':
+            self.winner = self.players[(self.turn + 1)%2]
             return True
         if self.board[0][2] == self.board[1][1] \
            and self.board[0][2] == self.board[2][0] \
            and self.board[0][2] != '_':
+            self.winner = self.players[(self.turn + 1)%2]
             return True
 
         return False
+
+    def getWinner(self):
+        return self.winner
+
 
     def getPos(self, r, c):
         return self.board[r][c];
--- games/servlet_tictactoe.py
+++ games/servlet_tictactoe.py
@@ -4,35 +4,50 @@ from pelix.ipopo.decorators import ComponentFactory, Property, Provides, \
 from pelix.http import HTTP_SERVLET, HTTP_SERVLET_PATH
 from pelix.http.routing import RestDispatcher, HttpGet, HttpPost, HttpPut
 
+
 @ComponentFactory()
 @Instantiate('tictactoe-servlet')
 @Requires("_srv", 'game_tictactoe_service')
 @Provides(HTTP_SERVLET)
 @Property('_path', 'pelix.http.path', "/tictactoe")
 class TicTacToeServletFactory(RestDispatcher):
-  def __init__(self):
-      super(TicTacToeServletFactory, self).__init__()
-      self._path = None
+    def __init__(self):
+        super(TicTacToeServletFactory, self).__init__()
+        self._path = None
+        self._srv = None
 
-  def bound_to(self, path, params):
-      return True
+    def bound_to(self, path, params):
+        return True
 
-  def unbound_from(self, path, params):
-      return None
+    def unbound_from(self, path, params):
+        return None
 
-  @HttpGet("/set/<x:int>/<y:int>")
-  def set_board(self, request, response, x, y):
-    self._srv.setPos(x, y);
-    self.show_board(request, response);
+    @HttpGet("/reset")
+    def reset(self, request, response):
+        self._srv.resetGame();
+        self.show_board(request, response);
 
-  @HttpGet("/")  
-  def show_board(self, request, response):
-      content = """<html>
+    @HttpGet("/set/<x:int>/<y:int>")
+    def set_board(self, request, response, x, y):
+        self._srv.setPos(x, y)
+        self.show_board(request, response)
+
+    @HttpGet("/")
+    def show_board(self, request, response):
+        cookie = request.get_header("Cookie")
+        content = """<html>
 <head>
 <meta charset="UTF-8"> 
 <title>Tic Tac Toe</title>
 <style>
-table {
+"""
+        if cookie:
+            content += """
+body {
+    background-color: tomato;
+}
+            """
+        content += """table {
     border-collapse: collapse;
     border-style: hidden;
     align: center;
@@ -48,24 +63,26 @@ table td {
 </style>
 </head>
 <body>
+<a href='/'>Home</a>
 <table>
 """
-      for x in range(3):
-        content += "<tr>\n"
-        for y in range(3):
-          value = self._srv.getPos(x, y)
-          content+= "<td><a href="
-          content+= '"/tictactoe/set/' + str(x) +'/' + str(y) + '"'
-          content+= ">" 
-          content+= value
-          content+= "</a></td>\n"
-        content += "</tr>\n"
-      content += """
+        for x in range(3):
+            content += "<tr>\n"
+            for y in range(3):
+                value = self._srv.getPos(x, y)
+                content += "<td><a href="
+                content += '"/tictactoe/set/' + str(x) + '/' + str(y) + '"'
+                content += ">"
+                content += value
+                content += "</a></td>\n"
+            content += "</tr>\n"
+        content += """
 </table>\n"""
-      if self._srv.checkWinner():
-          content+="<h2>GAME OVER</h2>\n"
-
-      content += """</body>
+        if self._srv.checkWinner():
+            content += "<h2>GAME OVER</h2>\n"
+            content += "<h5>Winner " + self._srv.getWinner() + "</h5>\n"
+            content += '<a href="/tictactoe/reset">reset<a/>\n'
+        content += """</body>
 </html>"""
 
-      response.send_content(200, content)
+        response.send_content(200, content)
